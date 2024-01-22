a309b90f2159e98a5bf7e5f724e73adc7afc168c
Robert Knight <robertknight@gmail.com>
2024-01-22 20:24:24 +0000

    Update ndarray -> rten tensor conversion for latest rten version
     - Update API usage for rten v0.3.0
     - Remove unnecessary `as_dyn` call.
       The `Input` type used for model inputs now impls `From<NdTensorView>`.
     - Read input width and height from model metadata

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use image::imageops::{resize, FilterType};
-use image::io::Reader as ImageReader;
-use ndarray::{s, Array2, Array4};
-
 use std::error::Error;
 use std::fs;
 
+use image::imageops::{resize, FilterType};
+use image::io::Reader as ImageReader;
+use ndarray::{s, Array2, Array4, Dimension};
 use rten::Model;
 use rten_tensor::prelude::*;
 use rten_tensor::{NdTensor, NdTensorView};
@@ -20,32 +19,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     let model =
         Model::load(&model_bytes).map_err(|err| format!("error loading model: {:?}", err))?;
 
-    // Resize to (224x224), which is a common sized used by many ImageNet models.
-    let [input_width, input_height] = [224, 224];
+    // Load image and resize to dimensions expected by model.
+    let (input_height, input_width) = match model.input_shape(0).unwrap().as_slice() {
+        &[_, _, rten::Dimension::Fixed(height), rten::Dimension::Fixed(width)] => (height, width),
+        _ => (224, 224), // ImageNet default
+    };
     let img = ImageReader::open(image_path)?.decode()?.into_rgb8();
-    let img = resize(&img, input_width, input_height, FilterType::Triangle);
+    let img = resize(
+        &img,
+        input_width as u32,
+        input_height as u32,
+        FilterType::Triangle,
+    );
 
     // Read pixels into NCHW tensor and apply standard ImageNet normalization.
     let imagenet_mean = [0.485, 0.456, 0.406];
     let imagenet_std_dev = [0.229, 0.224, 0.225];
-    let nchw_array = Array4::from_shape_fn(
-        [1, 3, input_height as usize, input_width as usize],
-        |(_n, c, y, x)| {
-            let value = img.get_pixel(x as u32, y as u32)[c] as f32 / 255.0;
-            (value - imagenet_mean[c]) / imagenet_std_dev[c]
-        },
-    );
+    let nchw_array = Array4::from_shape_fn([1, 3, input_height, input_width], |(_n, c, y, x)| {
+        let value = img.get_pixel(x as u32, y as u32)[c] as f32 / 255.0;
+        (value - imagenet_mean[c]) / imagenet_std_dev[c]
+    });
     let nchw_tensor: NdTensorView<f32, 4> = nchw_array
         .as_slice()
-        .map(|slice| {
-            let shape: [usize; 4] = nchw_array.shape().try_into().unwrap();
-            NdTensorView::from_slice(slice, shape, None).expect("incorrect slice length")
-        })
+        .map(|slice| NdTensorView::from_data(nchw_array.raw_dim().into_pattern().into(), slice))
         .expect("failed to convert ndarray");
 
     // Run model inference.
     let output = model
-        .run_one(nchw_tensor.as_dyn().into(), None)
+        .run_one(nchw_tensor.into(), None)
         .map_err(|err| format!("model run failed {:?}", err))?;
     let output: NdTensor<f32, 2> = output.try_into()?; // (batch, cls)
 
@@ -63,8 +64,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     let labels_json = fs::read_to_string("imagenet-simple-labels.json")?;
-    let labels: Vec<String> =
-        serde_json::from_str(&labels_json)?;
+    let labels: Vec<String> = serde_json::from_str(&labels_json)?;
     let top_label = labels.get(top_cls).map(|s| s.as_str()).unwrap_or("unknown");
 
     println!("Top class: {} (score: {})", top_label, top_score);
